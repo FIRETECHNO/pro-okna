39c7308948762a8d27751dba0f1735679f45f0c6
Jet-Green <grishadzyin@gmail.com>
2026-03-03 11:07:12 +0500

    add windows repair page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WindowsView from '@/views/WindowsView.vue'
 import BalconyView from '@/views/BalconyView.vue'
+import WindowsRepairView from '@/views/WindowsRepairView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,11 @@ const router = createRouter({
     path: "/balcony",
     name: "Balcony",
     component: BalconyView
+  },
+  {
+    path: "/windows-repair",
+    name: "WindowsRepair",
+    component: WindowsRepairView
   }
   ],
 })
