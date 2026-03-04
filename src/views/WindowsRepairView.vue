c3913bc31d4a4b79cc363124336f60216ded7f15
egkogladyshev <egkogladyshev@edu.hse.ru>
2026-03-05 01:11:06 +0500

    start WindowsRepairView, add components/BlockImageForWindowsRepairLg, BlockImageForWindowsRepairOther

--- src/views/WindowsRepairView.vue
+++ src/views/WindowsRepairView.vue
@@ -1,4 +1,17 @@
-<script setup></script>
+<script setup>
+import BlockImageForWindowsRepairLg from '@/components/BlockImageForWindowsRepairLg.vue'
+import BlockImageForWindowsRepairOther from '@/components/BlockImageForWindowsRepairOther.vue'
+</script>
 <template>
-
+  <v-container class="co" style="max-width: 100% !important; padding: 16px 0px">
+    <BlockImageForWindowsRepairLg class="d-none d-lg-flex"> </BlockImageForWindowsRepairLg>
+    <BlockImageForWindowsRepairOther class="d-flex d-lg-none"></BlockImageForWindowsRepairOther>
+  </v-container>
 </template>
+<style scoped>
+@media (max-width: 960px) {
+  .co {
+    margin-top: 94px;
+  }
+}
+</style>
