5628d41c5a0489e6eee94943e87eb6d82289466a
Jet-Green <grishadzyin@gmail.com>
2026-03-07 02:13:42 +0500

    add container

--- src/views/WindowsRepairView.vue
+++ src/views/WindowsRepairView.vue
@@ -4,13 +4,15 @@ import BlockImageForWindowsRepairOther from '@/components/BlockImageForWindowsRe
 import OurServicesForWindowsRepair from '@/components/OurServicesForWindowsRepair.vue'
 </script>
 <template>
-  <v-container class="co" style="max-width: 100% !important; padding: 16px 0px">
-    <BlockImageForWindowsRepairLg class="d-none d-lg-flex"> </BlockImageForWindowsRepairLg>
-    <BlockImageForWindowsRepairOther class="d-flex d-lg-none"></BlockImageForWindowsRepairOther>
-  </v-container>
-  <v-container>
-    <OurServicesForWindowsRepair></OurServicesForWindowsRepair>
-  </v-container>
+  <div>
+    <v-container class="co" style="max-width: 100% !important; padding: 16px 0px">
+      <BlockImageForWindowsRepairLg class="d-none d-lg-flex"> </BlockImageForWindowsRepairLg>
+      <BlockImageForWindowsRepairOther class="d-flex d-lg-none"></BlockImageForWindowsRepairOther>
+    </v-container>
+    <v-container>
+      <OurServicesForWindowsRepair></OurServicesForWindowsRepair>
+    </v-container>
+  </div>
 </template>
 <style scoped>
 @media (max-width: 960px) {
